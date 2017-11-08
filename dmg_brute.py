7ed5055d9b4703d932da5612f1e0bce454c98d04
Girish <girishameta@gmail.com>
2017-11-08 16:59:55 +0530

    hdiutil changes
    hdiutil verify is used instead of hdiutil mount to make it faster

--- dmg_brute.py
+++ dmg_brute.py
@@ -3,9 +3,10 @@ import os
 import sys
 import pexpect
 
+
 target_dmg = raw_input("Enter taget dmg file path: ")
 
-
+#file existance validation in if condition, when validated getting the file name out of path
 if not (os.path.exists(target_dmg.strip()) and os.path.isfile(target_dmg.strip())):
     print "File doesn't exist"
     exit()
@@ -16,24 +17,21 @@ else:
 
 
 word_list = raw_input("Enter wordlist file path: ")
-
+#file existance validation in if condition
 if not (os.path.exists(word_list.strip()) and os.path.isfile(word_list.strip())):
     print "File doesn't exist"
     exit()
 else:
+    # reading wordlist file line by line and checking for correct passoword using hdiutil binary
     with open(word_list.strip()) as passwords:
         for line in passwords:
-            child = pexpect.spawn('hdiutil mount '+target_dmg)
+            child = pexpect.spawn('hdiutil verify '+target_dmg)
             i = child.expect([pexpect.EOF,pexpect.TIMEOUT, 'Enter password to access "'+dmg_file_name+'"'])
-            if i == 0:
-                print("disk maybe already mounted")
-                exit()
-            else:
-                child.sendline(line)
+            child.sendline(line)
             child.expect(':')
-            i = child.expect([pexpect.EOF,'hdiutil: mount failed - Authentication error'])
+            i = child.expect([pexpect.EOF,'hdiutil: verify failed - Authentication error'])
             if i == 0:
-                print line.strip("\n") + " found :) check disk in mounted devices"
+                print line.strip("\n") + " found :)"
                 exit()
             else:
                 print line.strip("\n") + " not working"
